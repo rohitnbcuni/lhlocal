0bbfbc2c25c7803044f263545dab22b595cbb2a3
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-04-17 19:41:58 +0000

    import

---
+++ trunk/application/views/scripts/layouts/scripts/_includes/footer.php
@@ -0,0 +1,12 @@
+	<!----| START: Footer |---->
+	<div class="footer_container">
+		<!--<ul class="footer_nav">
+		<li></li>
+		</ul>
+		
+		<div class="legal"></div>-->
+	</div>
+	<!----| END: Footer |---->
+	
+</body>
+</html>
