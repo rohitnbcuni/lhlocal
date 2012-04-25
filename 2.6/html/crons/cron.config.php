bf323b5391173f8008e767b26756c7d835a88bee
shobhitsingh.bhadauria <shobhitS@users.noreply.github.com>
2012-04-25 12:21:12 +0000

    Add a cron config file

---
+++ 2.6/html/crons/cron.config.php
@@ -0,0 +1,11 @@
+<?php
+
+//Prod
+$rootPath = '/var/www/lighthouse-uxd/lighthouse/current';
+//QA
+$rootPath = '/var/www/lighthouse-uxd/qa/current';
+//Dev
+$rootPath = '/var/www/lighthouse-uxd/dev3/current';
+
+
+?>
