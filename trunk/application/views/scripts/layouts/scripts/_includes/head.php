bfa6de6b7ef8eb22656dd8b59bb2ce9617756a58
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-05-04 12:25:54 +0000

    LH#29876:Real Time Comment Updates

--- trunk/application/views/scripts/layouts/scripts/_includes/head.php
+++ trunk/application/views/scripts/layouts/scripts/_includes/head.php
@@ -146,7 +146,11 @@ if(isset($_SESSION['login_status']) && $_SESSION['login_status'] == "client"){
 							."\t<script src=\"/_js/workorders.create.js?" . $randNum . "\" type=\"text/javascript\"></script>\n"
 							."\t<script src=\"/_js/ajaxfileupload.js\" type=\"text/javascript\"></script>\n"
 							."\t<script src=\"/_js/jquery-ui-1.8.19.custom.min.js\" type=\"text/javascript\"></script>"
+							."\t<script src=\"/_js/jquery.ui.widget.js\" type=\"text/javascript\"></script>\n"
+							."\t<script src=\"/_js/jquery.notify.js\" type=\"text/javascript\"></script>\n"
 							. "\t<link href=\"/_css/jquery-ui.css?" . $randNum ."\" rel=\"stylesheet\" type=\"text/css\"/>";
+						echo  "<link href=\"/_css/ui.notify.css?" . $randNum ."\" rel=\"stylesheet\" type=\"text/css\"/>";	
+
 							
 						break;
 					}//Ticket #7927 Add new js file for calender view :wo_calender_view.js
