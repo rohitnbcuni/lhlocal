692f17eb2af35799312308f0e411260b492f1f62
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-06-06 14:00:59 +0000

    delete workorder not display on dashboard

--- branches/releases/2.9.0/html/_ajaxphp/next_new_comment.php
+++ branches/releases/2.9.0/html/_ajaxphp/next_new_comment.php
@@ -10,11 +10,12 @@
 		if(trim($woId) != ''){
 		$last_wid = $mysql->real_escape_string($_POST['last_wid']);
 		if($last_wid == ''){
-			$select_comments = "SELECT id, user_id FROM `workorder_comments` WHERE `workorder_id`='$woId' AND `active` = '1',`deleted` = '0' order by id LIMIT 0, 1";
+			$select_comments = "SELECT id, user_id FROM `workorder_comments` WHERE `workorder_id`='$woId' AND `active` = '1' AND `deleted` = '0' order by id LIMIT 0, 1";
 
 		}else{
-			$select_comments = "SELECT id, user_id FROM `workorder_comments` WHERE `workorder_id`='$woId' AND id > $last_wid  AND `active` = '1',`deleted` = '0'order by id LIMIT 0, 1";
+			$select_comments = "SELECT id, user_id FROM `workorder_comments` WHERE `workorder_id`='$woId' AND `active` = '1' AND `deleted` = '0' AND id > $last_wid  order by id LIMIT 0, 1";
 		}
+		//echo $select_comments;
 		$comm_result = @$mysql->query($select_comments);
 		$pattern = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";	
 		$comment_id =0;
