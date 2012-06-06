2f4f3574bb595fdd453842f7bc972d66b5e470dd
shobhitsingh.bhadauria <shobhitS@users.noreply.github.com>
2012-06-06 09:42:43 +0000

    delete workorder not display on dashboard

--- 2.9.0/html/_ajaxphp/workorder_json.php
+++ 2.9.0/html/_ajaxphp/workorder_json.php
@@ -191,7 +191,7 @@ if($workorder_result->num_rows > 0) {
 		}
 	}
 
-	$wo_last_comment = "SELECT wc.`id`,wc.`workorder_id`,wc.`user_id`,wc.`comment`,wc.`date` FROM `workorder_comments` wc, (select max(id) id from `workorder_comments` WHERE deleted ='0' group by workorder_id ) tab1,workorders b where wc.id=tab1.id and b.id = wc.workorder_id AND deleted ='0' $project_archive";
+	$wo_last_comment = "SELECT wc.`id`,wc.`workorder_id`,wc.`user_id`,wc.`comment`,wc.`date` FROM `workorder_comments` wc, (select max(id) id from `workorder_comments` group by workorder_id ) tab1,workorders b where wc.id=tab1.id and b.id = wc.workorder_id $project_archive";
 
 	$wo_last_comment_result = $mysql->query($wo_last_comment);
 
