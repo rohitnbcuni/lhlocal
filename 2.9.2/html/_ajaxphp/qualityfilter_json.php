b95815930b56573f8e3eb4e49c19a6cae4b394ac
shobhitsingh.bhadauria <shobhitS@users.noreply.github.com>
2012-06-26 14:28:52 +0000

    LH#31545 fixed

--- 2.9.2/html/_ajaxphp/qualityfilter_json.php
+++ 2.9.2/html/_ajaxphp/qualityfilter_json.php
@@ -177,6 +177,7 @@ if($project_result->num_rows > 0) {
 			  {
 			  
 		          $qa_project_version[$quality['version']] = htmlentities(substr($version_row['version_name'], 0, 10).$elipsee,ENT_QUOTES,'UTF-8');
+				  $qa_project_version_full[$quality['version']] = $version_row['version_name'];
 			  }
 		 }
 		 //For qa_project_iteration
@@ -337,7 +338,7 @@ if($project_result->num_rows > 0) {
           $assigned_result_origin = $mysql->query($select_qa_origin);
           $assigned_row_qa_origin = $assigned_result_origin->fetch_assoc();
 		 
-      array_push($postingList[$i]['quality'],Array('id' => $quality['id'], 'title' => htmlentities(substr($quality['title'], 0, 37).$elipse,ENT_QUOTES,'UTF-8'), 'full_title' => htmlentities($quality['title'],ENT_QUOTES,'UTF-8'), 'status' => $wo_status_array[$quality['status']],'severity' => $custom_feild_arr['QA_SEVERITY'][$quality['severity']],'severity_id' => $quality['severity'],'category' => $custom_feild_arr['QA_CATEGORY'][$quality['category']],'version' => $qa_project_version[$quality['version']], 'detected_by' => $detectedby, 'assigned_to' => $assigned,'assigned_to_id' => $quality['assigned_to'],'creation_date' => $quality['creation_date'] ,'open_date' => format_date($quality['creation_date']), 'assigned_date' => format_date($quality['assigned_date']), 'completed' => format_date($quality['completed_date']),'launch_date' => format_date($quality['launch_date']), 'class' => $dlClass, 'overdue_flag' => $overdue_flag,'wo_last_comment'=>nl2br(htmlentities($wo_last_comment,ENT_QUOTES,'UTF-8')),'wo_last_comment_user_id'=>$wo_last_comment_user_id,'wo_last_comment_user'=>$wo_last_comment_user ,'last_log_date'=>$log_date_val,'wo_last_comment_date'=>format_date($wo_last_comment_date),'qa_last_action'=>$qa_last_action ,'qa_requested_by'=>$requested_by, 'example_url'=>$quality['example_url'], 'body'=>preg_replace("/\s+/",' ',preg_replace("/[\r\n]+/", "\n",$quality['body'])),'active'=>$active,'deleted'=>$deleted,'archived'=>$archived,'closed_date'=> $closed_date,'browser'=>$assigned_row_qa_browser['field_name'],'os'=>$assigned_row_qa_os['field_name'],'origin'=>$assigned_row_qa_origin['field_name'],'iteration' =>$qa_project_iteration[$quality['iteration']],'product' =>$qa_project_product[$quality['product']] ));
+      array_push($postingList[$i]['quality'],Array('id' => $quality['id'], 'title' => htmlentities(substr($quality['title'], 0, 37).$elipse,ENT_QUOTES,'UTF-8'), 'full_title' => htmlentities($quality['title'],ENT_QUOTES,'UTF-8'), 'status' => $wo_status_array[$quality['status']],'severity' => $custom_feild_arr['QA_SEVERITY'][$quality['severity']],'severity_id' => $quality['severity'],'category' => $custom_feild_arr['QA_CATEGORY'][$quality['category']],'version' => $qa_project_version_full[$quality['version']],'version_break' => $qa_project_version[$quality['version']], 'detected_by' => $detectedby, 'assigned_to' => $assigned,'assigned_to_id' => $quality['assigned_to'],'creation_date' => $quality['creation_date'] ,'open_date' => format_date($quality['creation_date']), 'assigned_date' => format_date($quality['assigned_date']), 'completed' => format_date($quality['completed_date']),'launch_date' => format_date($quality['launch_date']), 'class' => $dlClass, 'overdue_flag' => $overdue_flag,'wo_last_comment'=>nl2br(htmlentities($wo_last_comment,ENT_QUOTES,'UTF-8')),'wo_last_comment_user_id'=>$wo_last_comment_user_id,'wo_last_comment_user'=>$wo_last_comment_user ,'last_log_date'=>$log_date_val,'wo_last_comment_date'=>format_date($wo_last_comment_date),'qa_last_action'=>$qa_last_action ,'qa_requested_by'=>$requested_by, 'example_url'=>$quality['example_url'], 'body'=>preg_replace("/\s+/",' ',preg_replace("/[\r\n]+/", "\n",$quality['body'])),'active'=>$active,'deleted'=>$deleted,'archived'=>$archived,'closed_date'=> $closed_date,'browser'=>$assigned_row_qa_browser['field_name'],'os'=>$assigned_row_qa_os['field_name'],'origin'=>$assigned_row_qa_origin['field_name'],'iteration' =>$qa_project_iteration[$quality['iteration']],'product' =>$qa_project_product[$quality['product']] ));
       }
     }
     $i=$i+1;
@@ -408,7 +409,7 @@ if(isset($from_action) && $from_action){
           $wo['body'] . "\t" .
           $wo['status'] . "\t " .
           $wo['severity'] . "\t " .
-          $wo['version'] . "\t " .
+          $wo['version_full'] . "\t " .
           $wo['browser'] . "\t " .
           $wo['os'] . "\t " .
           $wo['origin'] . "\t " .
