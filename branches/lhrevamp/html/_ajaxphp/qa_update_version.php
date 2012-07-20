9de9695b191934281381e86d5bdb8f725b5392c9
thiyagarajan.ramadoss@nbcuni.com <thiyagarajan.ramadoss@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-07-20 18:33:38 +0000

    Creating a new branch using 2.9.3 for lhrevamp

---
+++ branches/lhrevamp/html/_ajaxphp/qa_update_version.php
@@ -0,0 +1,29 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+	$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	$project_id= $_GET['project_id'] ;
+
+	$QRY_MASTER_SELECT ="SELECT `id`,`project_id`,`version_name` FROM `qa_project_version` where `project_id` in ('".$project_id."','0') order by project_id ='0' DESC, version_name ASC";
+
+	$result = $mysql->query($QRY_MASTER_SELECT);	
+	$versionList = Array();
+	if($result->num_rows > 0) {
+		$i = '0';
+		while($row = $result->fetch_assoc()) {
+			array_push($versionList,$row);
+			$i++;
+		}
+
+		  $jsonSettings = json_encode($versionList);
+
+		  // output correct header
+		  $isXmlHttpRequest = (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) ?
+		  (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ? true : false: false;
+		  ($isXmlHttpRequest) ? header('Content-type: application/json') : header('Content-type: text/plain');
+  	
+		  echo $jsonSettings;
+
+	}
+	
+?>
---
+++ branches/lhrevamp/html/email_test.php
@@ -0,0 +1,17 @@
+<?PHP
+
+//$to = 'vipul.dasaharish@igate.com';
+$to = 'Vipul.Harish@nbcuni.com';
+$subject = 'Lighthouse test mail please ignore';
+$msg = 'Lighthouse test mail please ignore';
+$headers = "From: "."LH"."\nMIME-Version: 1.0\nContent-type: text/html; charset=iso-8859-1";
+
+echo "email send start = ".date("Y-m-d : H:i:s")."<br>";
+if(mail($to, $subject, $msg, $headers)){
+	echo "email send end = ".date("Y-m-d : H:i:s");
+} else {
+	echo "sending failed";
+}
+
+
+?>
