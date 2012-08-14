e1d337a6252af4595758eb29cc711558d85cc8ba
manoj.sharma <manoj.sharma@nbcuni.com>
2012-08-14 18:24:45 +0000

    Fix For LH#23349

--- 2.9.10/html/_js/admin.js
+++ 2.9.10/html/_js/admin.js
@@ -557,9 +557,10 @@ function generateReport(){
 	var admin_to_select = $('#admin_to_select').val();
     var admin_to_year_select = $('#admin_to_year_select').val();
 	var admin_assign_select = $('#admin_assign_select').val();
+
 	if(admin_to_select!='' && admin_to_year_select!=''){
-	if(admin_user_select > admin_to_select && admin_year_select > admin_to_year_select || admin_user_select > admin_to_select && admin_year_select == admin_to_year_select || admin_user_select >= admin_to_select && admin_year_select > admin_to_year_select )
-	{ 
+	if(parseInt(admin_user_select) > parseInt(admin_to_select) && parseInt(admin_year_select) > parseInt(admin_to_year_select) || parseInt(admin_user_select) < parseInt(admin_to_select) && parseInt(admin_year_select) > parseInt(admin_to_year_select) || parseInt(admin_user_select) > parseInt(admin_to_select) && parseInt(admin_year_select) == parseInt(admin_to_year_select))
+	{ 	
 		  alert('Pls select correct date'); return false;
 	}
 	window.open('/_ajaxphp/admin_slareport.php?month='+admin_user_select+'&year='+admin_year_select+'&to_month='+admin_to_select+'&to_year='+admin_to_year_select+'&assign_to='+admin_assign_select );
