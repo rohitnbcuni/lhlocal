70473b79fbb5f781b8b7b928f687c9a7667ded83
manoj.sharma <manoj.sharma@nbcuni.com>
2012-10-08 15:26:38 +0000

    adding search module

---
+++ 2.11.0/application/modules/search/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?PHP
+
+	include('search.inc');
+	define('NBCDOTCOM' , 8);
+	class Search_IndexController extends LighthouseController { 
+		public function indexAction() {
+		$cnt = 5;
+             	//	$this->view->assign('calendar_yr',$year);
+		 print_r(SearchDisplay::Searchresult( $_POST['search_text'],$_POST['search_par']));
+		
+		}
+	}
+			
+?>
