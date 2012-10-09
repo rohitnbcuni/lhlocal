ae139bbfd519273cceb0744173d1905276fe921f
shobhitsingh.bhadauria <shobhitS@users.noreply.github.com>
2012-10-09 11:59:49 +0000

    Improved the code structure in search module

--- 2.11.0/application/modules/search/controllers/IndexController.php
+++ 2.11.0/application/modules/search/controllers/IndexController.php
@@ -5,9 +5,9 @@
 	class Search_IndexController extends LighthouseController { 
 		public function indexAction() {
 		$cnt = 5;
-             	//	$this->view->assign('calendar_yr',$year);
-		 SearchDisplay::Searchresult( $_POST['search_text'],$_POST['search_par']);
+        $searchResult = SearchDisplay::Searchresult( $_POST['search_text'],$_POST['search_par']);
 		
+		$this->view->assign("searchResult",$searchResult);
 		}
 	}
 			
