585e0cce502c12f2e318f6f99ac1140dff1167bd
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-10-10 13:19:43 +0000

    Fix the issue search dialog box

--- branches/releases/2.11.0/html/_js/search_box.js
+++ branches/releases/2.11.0/html/_js/search_box.js
@@ -7,11 +7,24 @@ function hide_searchpopup(){
       $('#popup_top').hide();
     }
 
+	
 $(document).ready(function(){	
+	$('div.bt_advSearch').mouseover(function(){
+	$('#popup_top').toggle();
+	
+	
+	}).mouseout(function(){
+	$('#popup_top').toggle();
+	
+	});
+	
+	
+	
 	$("#bt_search").click(function(){
 	
 		if($.trim($('#search_text').val()) == ''){
 			$('#search_text').focus();
+			$('#search_text').css('border-color', 'red');
 		
 		}else{
 			$('#search_box_form').submit();
