21feaab55abe3642961a6655ee3675a7964cd290
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-10-10 09:40:22 +0000

    fix the search box issues

--- branches/releases/2.11.0/html/_js/search_box.js
+++ branches/releases/2.11.0/html/_js/search_box.js
@@ -7,21 +7,16 @@ function hide_searchpopup(){
       $('#popup_top').hide();
     }
 
-/*function submit_form() {
-var frm = $('#search_box_form'); 
-var search_text = $("input#search_text").val();
-if ($("#search_par").is(':checked')){ 
-var option=$("#search_par").val();}else{var option='';};
-if ($("#search_par1").is(':checked')){
-var option1=$("#search_par1").val();}else{var option1 ='';};
-if ($("#search_par2").is(':checked')){
-var option2=$("#search_par2").val();}else{var option2='';};
-var dataString = 'search_text='+ search_text + '&search_par=' +option + '&search_par=' +option1+ '&search_par=' +option2;  
- $.ajax({
-    type: "POST",
-    url: "/search",
-    data: dataString,
-    success: function() {
-       $('#content_container').html(msg) }
-     });
-}*/
+$(document).ready(function(){	
+	$("#bt_search").click(function(){
+	
+		if($('#search_text').val() == ''){
+			$('#search_text').focus();
+		
+		}else{
+			$('#search_box_form').submit();
+		}
+	
+	});
+});	
+
