8b7a522db8d8de4519a47ba0bc3283fbd26279eb
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-11-05 12:34:44 +0000

    LH#35423

--- branches/releases/2.11.2/html/_ajaxphp/lock_week_time.php
+++ branches/releases/2.11.2/html/_ajaxphp/lock_week_time.php
@@ -1,15 +1,15 @@
 <?PHP 
 	include("../_inc/config.inc");
 	include("sessionHandler.php");
-	$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
-
-	$start_date = $_GET['start_date'];
-	$week_number = $_GET['week_number'];
-	$user_id = $_GET['user_id'];
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	global $mysql;
+	$start_date = $mysql->real_escape_string($_GET['start_date']);
+	$week_number = $mysql->real_escape_string($_GET['week_number']);
+	$user_id = $mysql->real_escape_string($_GET['user_id']);
 	$start_date_array = explode('-', $start_date);
 
 	$insertSql = "INSERT INTO `resource_planner_lock` (`user_id`, `week_num`, `year`, `start_date`) VALUES ('$user_id', '$week_number', '$start_date_array[0]', '$start_date')";
 
-	$mysql->query($insertSql);
+	$mysql->sqlordie($insertSql);
 	echo $mysql->insert_id;
 ?>
--- branches/releases/2.11.2/html/_ajaxphp/resource_planner_save.php
+++ branches/releases/2.11.2/html/_ajaxphp/resource_planner_save.php
@@ -2,7 +2,8 @@
 session_start();
 include('../_inc/config.inc');
 include("sessionHandler.php");
-$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+global $mysql;
 $postingList = Array();
 
 if($_SESSION['login_status'] == "client") {
@@ -14,11 +15,11 @@ if($_SESSION['login_status'] == "client") {
 
 if (@$_POST["overtime"]) {
 //if (@$_GET["overtime"]) {
-	$projectId = $_POST['projectid'];
-	$hours = $_POST['hours'];
+	$projectId = $mysql->real_escape_string($_POST['projectid']);
+	$hours = $mysql->real_escape_string($_POST['hours']);
 	$notes = $mysql->real_escape_string($_POST['notes']);
-	$userId = $_POST['userid'];
-	$date = $_POST['date'];
+	$userId = $mysql->real_escape_string($_POST['userid']);
+	$date = $mysql->real_escape_string($_POST['date']);
 	$date_part = explode("-", $date);
 	$dateFormat = $date_part[2] ."/" .$date_part[0] ."/" .$date_part[1];
 	
@@ -37,17 +38,17 @@ if (@$_POST["overtime"]) {
 	//$dateFormat = $date_part[2] ."/" .$date_part[0] ."/" .$date_part[1];
 
 	$sql = "SELECT * FROM resource_blocks WHERE userid='$userId' AND daypart='9' AND datestamp='$dateFormat'";
-	$res = $mysql->query($sql);
+	$res = $mysql->sqlordie($sql);
 	if ($res->num_rows > 0) {
 		$rb = $res ->fetch_assoc();
 		$id = $rb['id'];
 		$sql = "UPDATE resource_blocks SET projectid=$projectId,notes='$notes',hours='$hours' WHERE id='$id'";
 		//echo $sql;
-		$mysql->query($sql);
+		$mysql->sqlordie($sql);
 	} else {
 		$sql = "INSERT INTO resource_blocks (userid,projectid,daypart,datestamp,hours,notes,status) VALUES ('$userId',$projectId,'9','$dateFormat','$hours','$notes','4')";
 		//echo $sql;
-		$mysql->query($sql);
+		$mysql->sqlordie($sql);
 	}
 } else {
 	$newData = str_replace("\\", "", $_POST['data']);
@@ -70,10 +71,10 @@ if (@$_POST["overtime"]) {
 
 		if ($status==0) {
 			$sql = "DELETE FROM resource_blocks WHERE userid='$user' AND daypart='" .$dayblock['block'] ."' AND datestamp='$datestamp'";
-			$res = $mysql->query($sql);
+			$res = $mysql->sqlordie($sql);
 		} else {
 			$sql = "SELECT * FROM resource_blocks WHERE userid='$user' AND daypart='" .$dayblock['block'] ."' AND datestamp='$datestamp'";
-			$res = $mysql->query($sql);
+			$res = $mysql->sqlordie($sql);
 			if ($res->num_rows > 0) {
 				if ($status<5) {
 					$rb = $res ->fetch_assoc();
@@ -87,13 +88,13 @@ if (@$_POST["overtime"]) {
 						$sql .= ", projectid='".$projectID."'";
 					}
 					$sql .= " WHERE id='$id'";
-					$mysql->query($sql);
+					$mysql->sqlordie($sql);
 				} else {
 					$rb = $res ->fetch_assoc();
 					$id = $rb['id'];
 					if ($rb['projectid']) {
 						$sql = "UPDATE resource_blocks SET status='4' WHERE id='$id'";
-						$mysql->query($sql);
+						$mysql->sqlordie($sql);
 					}
 
 				}
@@ -106,7 +107,7 @@ if (@$_POST["overtime"]) {
 						$sql .= "'".$projectID."'";
 					}
 					$sql .=",'" .$dayblock['block'] ."','$status','$datestamp')";
-					$mysql->query($sql);
+					$mysql->sqlordie($sql);
 				}
 			}
 		}
@@ -122,7 +123,7 @@ $isXmlHttpRequest = (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) ?
   (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ? true : false: false;
 ($isXmlHttpRequest) ? header('Content-type: application/json') : header('Content-type: text/plain');
 
-echo $jsonSettings."<br />\n";
+echo $jsonSettings;
 //echo $sql."<br />\n";
 
 ?>
