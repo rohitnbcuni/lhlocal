652b750356119cb49a540acf7e80aed3adb0c2f7
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-11-19 13:39:24 +0000

    Deleted Unused files. and LH#35897

--- branches/releases/2.11.3/html/healthCheck.php
+++ branches/releases/2.11.3/html/healthCheck.php
@@ -5,7 +5,7 @@ include('_inc/config.inc');
 	
 	$link = mysql_connect(DB_SERVER.":".DB_PORT, DB_USERNAME, DB_PASSWORD);
 	if (!$link) {
-	    die('Could not connect: ' . mysql_error());
+	    die('Could not connect: ');
 	}else{
 		echo 'GOOD';
 	}
@@ -14,6 +14,6 @@ include('_inc/config.inc');
 	// make DB_DATABASE the current db
 	$db_selected = mysql_select_db(DB_DATABASE, $link);
 	if (!$db_selected) {
-	    die ('Can\'t use Database : ' . mysql_error());
+	    die ('Can\'t use Database : ');
 	}
 	mysql_close($link);
