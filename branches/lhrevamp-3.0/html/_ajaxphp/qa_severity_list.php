110ed445ffb527aa308631c1fc4cb27eb06cb127
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-12-07 08:49:14 +0000

    creating new repo for LH revamp

---
+++ branches/lhrevamp-3.0/html/_ajaxphp/qa_severity_list.php
@@ -0,0 +1,22 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	//Defining Global mysql connection values
+	global $mysql;
+	$feildKEY = $_GET['feildKEY'];	
+
+	$QRY_MASTER_SELECT ="SELECT `field_name`,`field_id` FROM `lnk_custom_fields_value` cfv,`lnk_custom_fields` cf where cfv.`field_key` ='".$feildKEY."' and cfv.field_key = cf.field_key and cf.active='1' and cf.deleted='0' and cfv.active='1' and cfv.deleted='0' order by sort_order";
+
+	$fields_list = $mysql->sqlordie($QRY_MASTER_SELECT);
+	$selected = "";
+	if($fields_list->num_rows > 0) {
+		while($row = $fields_list->fetch_assoc()) {
+			$html .= '<option value="' .$row['field_id'] .'"' .$selected .'>' 
+							.$row['field_name'].'</option>';
+		}
+	}  
+	
+	echo $html;
+?>
