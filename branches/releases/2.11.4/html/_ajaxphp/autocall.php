b1017367294851a3857bbb5bd74d24312f0adee8
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2012-12-10 18:25:24 +0000

    enable autosearch

--- branches/releases/2.11.4/html/_ajaxphp/autocall.php
+++ branches/releases/2.11.4/html/_ajaxphp/autocall.php
@@ -1,7 +1,7 @@
 <?php 
- $results = array();
+$results = array();
 $SOLR_URL_STRING = "http://ec2-75-101-162-191.compute-1.amazonaws.com:8080/solr/lighthouse_active/select?q=";
-$url = $SOLR_URL_STRING.urlencode($_REQUEST['letters']).'&featureClass=P&style=full&maxRows=10&name_startsWith='.$_REQUEST[name_startsWith];
+$url = $SOLR_URL_STRING.urlencode($_REQUEST['letters']).'&featureClass=P&style=full&maxRows=100&name_startsWith='.$_REQUEST['letters'];
 
 			$ch = curl_init();
 			$request='<request>'.$request.'</request>';
@@ -24,19 +24,57 @@ $url = $SOLR_URL_STRING.urlencode($_REQUEST['letters']).'&featureClass=P&style=f
 			curl_close($ch);
 
 $results = new SimpleXMLElement($response);
-			
-if(count($results) > 0){
+/*if(count($results) > 0){
 	foreach($results['result'] as $key => $val){
 		//$inf["ID"]."###".$inf["countryName"]."|";
 		
-		//print_r($val);
+		print_r($val);
 		echo $val->arr->str[0]."###".$val->arr->str[0]."|";
 		
 	
+	}*/
+
+if(count($results) > 0){
+	foreach($results as $key =>$val){
+		if($key == 'result'){
+			$result_found = $val['numFound'];
+			$searchResult =  $val->doc;
+			if(count($searchResult) > 0){
+			foreach($searchResult as $k => $v){
+					if(count($searchResult) > 0){
+						foreach($searchResult as $sKey => $eVal){
+						/* $comments='';
+						for($i=0;$i<count($eVal->arr[1]->str);$i++)
+						{$no = $i+1;
+						$comments .= $no.'.'. $eVal->arr[1]->str[$i].'</br>';
+						}*/
+						$id[] =   $eVal->long[0];
+						$project_id[] =  $eVal->str[1];
+					        $title[] = $eVal->str[2];
+						$urllink[] =  $eVal->arr->str[1];
+						$desc[] =  $eVal->str[4];
+						$cat[] =  $eVal->str[5];
+						/* $comment[] =  (array) $comments ;*/
+						if( $eVal->str[5]=='quality'){
+						$count_qa=$i++;}
+						else{$count_wo=$wo++; }
+
+						}
+
+					}
+				}
+
+			}
+		}
+
 	}
+}
 
-
-} 			
+for($i =0 ; $i<count($id); $i++){
+echo $id[$i][0]."###".ucfirst($title[$i][0])."|"; 
+// echo $title[$i][0];
+}
+ 			
 
 
 ?>
