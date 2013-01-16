77aecdd8854d2f8953a40404ff152170d79d8765
shobhitsingh.bhadauria <shobhitS@users.noreply.github.com>
2013-01-16 14:30:33 +0000

    ordering issue

--- 2.11.5/application/modules/search/controllers/IndexController.php
+++ 2.11.5/application/modules/search/controllers/IndexController.php
@@ -10,13 +10,12 @@
 			$request = $this->getRequest();
 			$search_par = $request->getParam('search_par');
 			$search_text = $request->getParam('search_text');
-						
 			$searchResult = SearchDisplay::Searchresult( $search_text,$search_par);
-			setcookie("search_text", $search_text, time()+3600);
-			setcookie("search_par", $search_par, time()+3600);
+			@setcookie("search_text", $search_text, time()+3600);
+			@setcookie("search_par", $search_par, time()+3600);
 			$this->view->assign("searchResult",$searchResult);
 			$this->view->assign("search_text",$search_text);
-			 $this->view->assign("search_par",$search_par);
+			$this->view->assign("search_par",$search_par);
 		}
 	}
 			
