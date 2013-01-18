ff8bd08410cb6d3caeaf73fa85e293ee8a8fbfc5
neeraj.virmani@nbcuni.com <neeraj.virmani@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2013-01-18 09:46:30 +0000

    creating new tag

---
+++ tags/2.11.6.0/html/_ajaxphp/autocall.php
@@ -0,0 +1,103 @@
+<?php 
+include('../_inc/config.inc');
+include("sessionHandler.php");
+$results = array();
+//$search_str = trim(str_replace('"','&quot;',$_GET['letters']));
+$search_str = htmlspecialchars($_GET['letters']);
+if($search_str != ''){
+		$search_string = '"'.urlencode($search_str).'"';
+		$search_par = trim($_GET['filters']);
+		if($search_par=='All'){
+			$string='';
+		}
+		elseif($search_par=='Defect'){
+			$string='%20AND%20categories:quality';
+		}
+		elseif($search_par=='WorkO'){
+			$string='%20AND%20categories:workorder';
+		}		
+		$int_id = (int) $search_str;
+		$doc_id = (!empty($int_id))?"%20OR%20docid:".$int_id:'';
+		$url = SOLR_URL_STRING.'((title:'.$search_string.'%20OR%20description:'.$search_string.$doc_id.')'.$string.')&featureClass=P&style=full&start=0&rows=10&sort=docid%20desc&name_startsWith='.$search_string;
+
+		$ch = curl_init();
+		$request='<request>'.$request.'</request>';
+		curl_setopt($ch, CURLOPT_URL, $url);
+		if(defined('HTTP_PROXY') && HTTP_PROXY!='')
+		{
+				curl_setopt($ch,CURLOPT_PROXY,HTTP_PROXY);
+				curl_setopt($ch,CURLOPT_HTTPPROXYTUNNEL,true);
+				curl_setopt($ch,CURLOPT_PROXYTYPE,CURLPROXY_HTTP);
+		}
+		
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt ($ch, CURLOPT_POST, 1);
+		curl_setopt ($ch, CURLOPT_POSTFIELDS, $request);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/xml', 'Content-Type: application/xml'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_USERPWD,BC_USER . ":" . BC_PASSWORD);
+		if(preg_match('/^(http)/',$url)) curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+		$response = curl_exec($ch);
+		curl_close($ch);
+			
+		if(ISSET($response) && strlen($response) > 10){
+			$results = new SimpleXMLElement($response);
+		}
+		/*if(count($results) > 0){
+			foreach($results['result'] as $key => $val){
+				//$inf["ID"]."###".$inf["countryName"]."|";
+				
+				print_r($val);
+				echo $val->arr->str[0]."###".$val->arr->str[0]."|";
+				
+			
+			}*/
+
+		if(count($results) > 0){
+			foreach($results as $key =>$val){
+				if($key == 'result'){
+					$result_found = $val['numFound'];
+					$searchResult =  $val->doc;
+					if(count($searchResult) > 0){
+					foreach($searchResult as $k => $v){
+							if(count($searchResult) > 0){
+								foreach($searchResult as $sKey => $eVal){
+								/* $comments='';
+								for($i=0;$i<count($eVal->arr[1]->str);$i++)
+								{$no = $i+1;
+								$comments .= $no.'.'. $eVal->arr[1]->str[$i].'</br>';
+								}*/
+								$id[] =   $eVal->long[0];
+								$project_id[] =  $eVal->str[1];
+									$title[] = $eVal->str[2];
+								$urllink[] =  $eVal->arr->str[1];
+								$desc[] =  $eVal->str[4];
+								$cat[] =  $eVal->str[5];
+								/* $comment[] =  (array) $comments ;*/
+							/*	if( $eVal->str[5]=='quality'){
+								$count_qa=$i++;}
+								else{$count_wo=$wo++; }
+								*/
+								}
+
+							}
+						}
+
+					}
+				}
+
+			}
+		}
+		$pageURL = BASE_URL;
+		for($i =0 ; $i<count($id); $i++){
+		if($cat[$i][0]=='quality'){$link = $pageURL.'/quality/index/edit/?defect_id=';}else{$link = $pageURL.'/workorders/index/edit/?wo_id='; }
+		echo $id[$i][0]."###".'<a  href="'.$link.$id[$i][0].'">'.ucfirst($title[$i][0]).'<a/>'."|"; 
+		// echo $title[$i][0];
+		}
+	}
+ 			
+
+
+?>
+
