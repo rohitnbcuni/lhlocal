f26a2fa3998e3e8bf6772c5b4e25e5fc3e9a4bb7
praveen.anthu <praveen.anthu@nbcuni.com>
2013-03-11 12:44:00 +0000

    multiple budget code save changes

--- html/_ajaxphp/update_finance.php
+++ html/_ajaxphp/update_finance.php
@@ -6,35 +6,168 @@
 	$project = $mysql->real_escape_string(@$_GET['project_id']);
 	$budget_code = $mysql->real_escape_string(@$_GET['budget_code']);
 	
-	$update_budget = "UPDATE `projects` SET `budget_code`='$budget_code' WHERE `id`='$project'";
-	$mysql->sqlordie($update_budget);
+	$insert_new_budget_code = "INSERT INTO `project_budget` (project_id,budget_code,total_budget,quarter1_budget,quarter2_budget,quarter3_budget,quarter4_budget,note,updated_by,updated_on) VALUES"; 
 	
 	$fin = @$_GET['finance'];
 	$fin_keys = array_keys($fin);
+	$final_response = "";
 
+	$user_id = $_SESSION['user_id'];
+
+	
+	#Saving the note & Updating the database
+	if(@$_GET['action'] == "savenote") {
+		$update_id = "";$note = "";
+		$update_id = @$_GET['budget_update_id'];
+		$note = @$_GET['note'];
+
+		if($update_id != "" && trim($note) != "") {
+				$update_budget = "UPDATE `project_budget` SET `note` = '".$note."' WHERE `id` IN(".$update_id.")";
+				//echo $update_budget;
+				$result = $mysql->sqlordie($update_budget);
+		}
+		exit;
+	}
+
+	
+	#Setting the flag for the Original Budget Code
+	if(@$_GET['action'] == "setFlag") {
+
+		if(trim($project) != "") {
+				$update_budget = "UPDATE project_budget SET delete_flag = 1 WHERE project_id=".$project." AND id NOT IN (SELECT * from (SELECT max(`id`) AS `id` FROM `project_budget` where project_id=".$project." GROUP BY `budget_code` ORDER BY `updated_on` limit 1) as ids)";
+				//echo $update_budget;
+				$result = $mysql->sqlordie($update_budget);
+		}
+		exit;
+	}
+
+
+	#Handling Multiple Budget Code Save
 	if(@$_GET['action'] == "save") {
-		$totalBudget = $mysql->real_escape_string(@$_GET['totalBudget']);
-		$quarterOneBudget = $mysql->real_escape_string(@$_GET['quarter1']);
-		$quarterTwoBudget = $mysql->real_escape_string(@$_GET['quarter2']);
-		$quarterThreeBudget = $mysql->real_escape_string(@$_GET['quarter3']);
-		$quarterFourBudget = $mysql->real_escape_string(@$_GET['quarter4']);
+
+		if(@$_GET['requestFrom'] == "budget") {
+
+		$budget_array = "";
+		$budget_code = "";
+
 		$select_budget = "SELECT * FROM project_budget WHERE `project_id`='$project' LIMIT 1";
 		$budget_result = $mysql->sqlordie($select_budget);
+
+		$budget_array = @$_GET['budget'];
+		$budget_array = array_values($budget_array);
+		$budget_code = @$_GET['budgetcode'];
+
 		if($budget_result->num_rows == 1){
-			if($totalBudget > 0){
-				$row = $budget_result->fetch_assoc();
-				$update_project_budget = "UPDATE project_budget SET total_budget='" 
-						.$totalBudget ."',quarter1_budget='" .$quarterOneBudget."',quarter2_budget='" .$quarterTwoBudget."',quarter3_budget='" .$quarterThreeBudget."',quarter4_budget='" .$quarterFourBudget."' WHERE `id`='" .$row['id'] ."'";
-				@$mysql->sqlordie($update_project_budget);
-			}
+			//if(1){
+				//$row = $budget_result->fetch_assoc();
+				//$budget_array['note'] = "test";
+
+				if(is_array($budget_array) && count($budget_array) > 0)	
+				{
+					foreach($budget_array as $key => $budget) {	
+
+						$result = "";$bc_response = "";
+
+						$check_recent_budget = "SELECT * FROM `project_budget` WHERE `project_id`='$project' AND `budget_code`='".trim($budget_code[$key]["original_budget_code"])."' ORDER BY `updated_on` DESC LIMIT 1";
+						$result = $mysql->sqlordie($check_recent_budget);
+						$bc_response = $result->fetch_assoc();
+						
+						if(trim($bc_response["total_budget"]) == trim($budget['totalBudget']) && trim($bc_response["quarter1_budget"]) == trim($budget['quarter1']) && trim($bc_response["quarter2_budget"]) == trim($budget['quarter2']) && trim($bc_response["quarter3_budget"]) == trim($budget['quarter3']) && trim($bc_response["quarter4_budget"]) == trim($budget['quarter4'])) {
+							continue;
+						}else {
+								$check_new_budget = "SELECT `id` FROM `project_budget` WHERE `project_id`='$project' AND `budget_code`='".trim($budget_code[$key]["original_budget_code"])."'";
+								$new_budget_result = $mysql->sqlordie($check_new_budget);
+	
+								if($new_budget_result->num_rows > 0) {
+								
+									$insert_project_budget = "INSERT INTO project_budget "	."(project_id,total_budget,quarter1_budget,quarter2_budget,quarter3_budget,quarter4_budget,updated_by,updated_on,budget_code,note)"
+												."VALUES "
+												."('$project','" .trim($budget['totalBudget'])."','" .trim($budget['quarter1'])."','" .trim($budget['quarter2'])."','" .trim($budget['quarter3'])."','" .trim($budget['quarter4'])."'," .trim($user_id).",NOW(),'".trim($budget_code[$key]["original_budget_code"])."','')";
+									//echo $insert_project_budget;
+									@$mysql->sqlordie($insert_project_budget);
+
+									$recent_id = "";
+									$obj = "";
+									$recent_id_sql = "SELECT MAX(id) as id FROM `project_budget` WHERE `project_id`='$project'";
+
+									$obj = @$mysql->sqlordie($recent_id_sql);
+									$recent_id = $obj->fetch_assoc();
+									$final_response .= $recent_id["id"].",";
+							}else {
+									$insert_project_budget = "INSERT INTO project_budget "	."(project_id,total_budget,quarter1_budget,quarter2_budget,quarter3_budget,quarter4_budget,updated_by,updated_on,budget_code,note)"
+												."VALUES "
+												."('$project','" .trim($budget['totalBudget'])."','" .trim($budget['quarter1'])."','" .trim($budget['quarter2'])."','" .trim($budget['quarter3'])."','" .trim($budget['quarter4'])."'," .trim($user_id).",NOW(),'".trim($budget_code[$key]["original_budget_code"])."','')";
+									//echo $insert_project_budget;
+									@$mysql->sqlordie($insert_project_budget);
+							}
+							
+						}
+
+					}
+
+					if($final_response != "") {
+						$final_response = rtrim($final_response,",");
+					}
+
+					if(is_array($budget_code) && count($budget_code) > 0) {
+						foreach($budget_code as $value) {
+							$update_query = "";
+							$update_query = "UPDATE `project_budget` SET `budget_code`='".trim($value["budget_code"])."' WHERE `project_id`=".$project." AND `budget_code`='".trim($value["original_budget_code"])."'";
+							$update_query_result = $mysql->sqlordie($update_query);
+						}
+					}
+
+				}
+			//}
 			
 		}else if($budget_result->num_rows == 0){
-			$insert_project_budget = "INSERT INTO project_budget "
-						."(project_id,total_budget,quarter1_budget,quarter2_budget,quarter3_budget,quarter4_budget) "
-						."VALUES "
-						."('$project','" .$totalBudget ."','" .$quarterOneBudget ."','" .$quarterTwoBudget ."','" .$quarterThreeBudget ."','" .$quarterFourBudget ."')";
-			@$mysql->sqlordie($insert_project_budget);
+
+			if(is_array($budget_array) && count($budget_array) > 0)	
+			{
+				foreach($budget_array as $key => $budget) {	
+
+				$insert_project_budget = "INSERT INTO project_budget "	."(project_id,total_budget,quarter1_budget,quarter2_budget,quarter3_budget,quarter4_budget,updated_by,updated_on,budget_code,note)"
+							."VALUES "
+							."('$project','" .$budget['totalBudget'] ."','" .$budget['quarter1'] ."','" .$budget['quarter2'] ."','" .$budget['quarter3'] ."','" .$budget['quarter4'] ."'," .$user_id .",NOW(),'".trim($budget_code[$key]["budget_code"])."','')";
+							//echo $insert_project_budget;
+				@$mysql->sqlordie($insert_project_budget);
+				}
+			}
 		}
+
+		/*
+		$budgetCodeCount = @$_GET['budget_count'];
+		$budgetQuery = "";
+
+		#Inserting the newly added Budget Code
+		if($budgetCodeCount > 0) {
+			for($budgetLoop=1;$budgetLoop<=$budgetCodeCount;$budgetLoop++) {
+				$bc = "";
+				
+				if(@$_GET['budget_code_'.$budgetLoop] != "") {
+					$bc = explode(",",@$_GET['budget_code_'.$budgetLoop]);
+				}
+				$budgetQuery = $budgetQuery.$project.",";
+
+				if(is_array($bc) && count($bc) > 0) {
+					foreach($bc as $budgetValue) {
+						if(!is_numeric($budgetValue)) {
+							$budgetQuery .= "'".$budgetValue."',";
+						}else{
+							$budgetQuery .= $budgetValue.",";
+						}
+					}
+				}
+
+				$budgetQuery .= "NOW()";
+				$budgetQuery .= "),(";			
+			}
+				$budgetQuery = rtrim($budgetQuery, ",(");
+				$insert_new_budget_code .= " (".$budgetQuery;
+				@$mysql->sqlordie($insert_new_budget_code);				
+		}*/
+	}
+
 		for($i = 0; $i < sizeof($fin_keys); $i++) {
 			if(isset($fin[$fin_keys[$i]])) {
 				$select_finance = "SELECT * FROM `project_phase_finance` WHERE `project_id`='$project' AND `phase`='" .$fin[$fin_keys[$i]]['phase'] ."' LIMIT 1";
@@ -114,4 +247,6 @@
 			}
 		}
 	}
+
+	echo $final_response;
 ?>
