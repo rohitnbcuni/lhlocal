967fbbe0c5906d875807c81c00f6495c6db17087
praveen.anthu@nbcuni.com <praveen.anthu@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2013-03-19 11:51:05 +0000

    query change to retrieve most recent phase entry from project_phases table

--- branches/lhrevamp4.0/html/_ajaxphp/quickfilter_json_detail.php
+++ branches/lhrevamp4.0/html/_ajaxphp/quickfilter_json_detail.php
@@ -49,7 +49,7 @@
 		$phase_data_res = @$mysql->sqlordie($select_phase_data);
 		$phase_data_row = @$phase_data_res->fetch_assoc();
 		
-		$timeline_query = "SELECT * FROM `project_phases` WHERE `project_id`='" .$mysql->real_escape_string($_GET['projID'])."' AND `phase_type`='" .$row_phases['phase'] ."' LIMIT 1";
+		$timeline_query = "SELECT * FROM `project_phases` WHERE `project_id`='" .$mysql->real_escape_string($_GET['projID'])."' AND `phase_type`='" .$row_phases['phase'] ."' order by id DESC LIMIT 1";
 		$timeline_res = @$mysql->sqlordie($timeline_query);
 		$timeline_row = @$timeline_res->fetch_assoc();
 		
