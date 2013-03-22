4efc47ac2e903d224b74bd399972a19fa5da09ac
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2013-03-22 13:21:03 +0000

    Search log

---
+++ branches/releases/2.11.10/html/_ajaxphp/solr_search_log.php
@@ -0,0 +1,37 @@
+<?php
+
+include('../_inc/config.inc');
+include("sessionHandler.php");
+include('../_ajaxphp/rally_function.php');
+//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+global $mysql;
+$result = $mysql->sqlordie("SELECT * FROM search_log");
+$OUTPUT .= "User Name, Search Strings\n";
+while($row = $result->fetch_assoc()){
+	$OUTPUT .= $row['user_id'].",";
+	$OUTPUT .= $row['pattern']."\n";
+	
+
+}
+
+
+	
+
+
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: private",false);
+// This one allows display or download
+header("Content-Type: application/octet-stream");
+// This one forces a download - uncomment (and comment octet-stream) if you want that
+//header("Content-Type: application/vnd.ms-excel");
+header("Content-Disposition: attachment; filename=\"search_log.csv\";" );
+header("Content-Transfer-Encoding: binary"); 
+//print $header; 
+print $OUTPUT;
+
+
+
+?>
+
