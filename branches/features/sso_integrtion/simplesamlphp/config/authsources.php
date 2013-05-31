257dc065f75d377be4205567260ff9d73d5a7648
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2013-05-31 09:19:52 +0000

    SP changes

--- branches/features/sso_integrtion/simplesamlphp/config/authsources.php
+++ branches/features/sso_integrtion/simplesamlphp/config/authsources.php
@@ -4,12 +4,13 @@ $config = array(
 /* This is the name of this authentication source, and will be used to access it later. */
     "nbcu-sp" => array(
         'saml:SP',
+	 'RelayState' => 'http://dev3.lighthouse.nbcuots.com',
         'idp' => 'nbcufssstg',
         'entityID' => 'lighthouse',
-        'RelayState' => 'http://dev3.lighthouse.nbcuots.com',
      //   'privatekey' => 'STAR_ lighthouse.com.key', /* if you have*/
       // 'certificate' => 'STAR_ lighthouse.com.crt', /* if you have*/
-        'NameIDPolicy' => NULL
+        'NameIDPolicy' => NULL,
+	'ReturnTo' => 'http://dev3.lighthouse.nbcuots.com'
     ),
 );
 
