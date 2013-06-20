27ca218486add425d10fe0a5fa5508980b77df82
manoj.sharma <manoj.sharma@nbcuni.com>
2013-06-20 13:13:42 +0000

    createing new branch 2.11.13

---
+++ 2.11.13/html/_ajaxphp/add_QCdefault_cc.php
@@ -0,0 +1,56 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+		$projectId = $mysql->real_escape_string($_GET['projectId']);
+
+		$cc = $mysql->real_escape_string($_GET['cc']);
+		
+		$ccArray = array();
+		$list = explode(",", $cc);
+
+
+
+
+             for($i = 0; $i < sizeof($list); $i++) {
+				if(!empty($list[$i]) && !isset($ccArray[$list[$i]])) {
+					if($list[$i] != @$_GET['remove'])
+					$ccArray[$list[$i]]=true;
+				}
+			}
+
+                 $listKeys = array_keys($ccArray);
+			$arrayData = "";
+			
+			for($z = 0; $z < sizeof($listKeys); $z++) {
+   				 $arrayData .= $listKeys[$z] .",";
+			}
+			
+			$update_cc = "UPDATE `projects` SET `qccclist`='$arrayData' WHERE `id`='$projectId'";
+			@$mysql->sqlordie($update_cc);
+
+                     $select_cc = "SELECT `qccclist` FROM `projects` WHERE `id`='$projectId' LIMIT 1";
+			 $result = @$mysql->sqlordie($select_cc);
+			$row = @$result->fetch_assoc();
+
+			if($result->num_rows > 0) {
+				$new_list = explode(",", $row['qccclist']);
+				$list = "";
+				
+				for($x = 0; $x < sizeof($new_list); $x++) {
+					if(!empty($new_list[$x])) {
+						$select_cc_user = "SELECT * FROM `users` WHERE `id`='" .$new_list[$x] ."' LIMIT 1";
+						$cc_user_result = @$mysql->sqlordie($select_cc_user);
+						$cc_user_row = @$cc_user_result->fetch_assoc();
+						
+						$list .= '<li class="admincc_listli">'
+                                         ."<div class=\"admincclist_name\">" .ucfirst($cc_user_row['first_name']) ." " .ucfirst($cc_user_row['last_name']) ."</div>"
+                                               	."<button class=\"status admincclist_remover\" onClick=\"removeqcCcUser(" .$new_list[$x] .",'".$projectId."'); return false;\"><span>remove</span></button>"
+								."</li>";
+					}
+				}				
+				echo $list;				
+			}
+
+               
+?>
---
+++ 2.11.13/html/_ajaxphp/update_project_status.php
@@ -0,0 +1,37 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+	global $mysql;
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	
+	$project_id = $mysql->real_escape_string(@$_GET['project_id']);
+	$project_status_id = $mysql->real_escape_string(@$_GET['project_status_id']);
+	$userID = $mysql->real_escape_string(@$_GET['user_id']);
+	$html = '';
+	
+	$project_sql = 'SELECT IFNULL(project_status, 0) AS status FROM projects WHERE id="' . $project_id . '"';
+	$projectResult = $mysql->sqlordie($project_sql);
+	$projectRow = $projectResult->fetch_assoc();
+	
+	$project_status_sql = 'SELECT * FROM lnk_project_status_types WHERE id="' . $project_status_id . '"';
+	$projectStatusResult = $mysql->sqlordie($project_status_sql);
+	$projectStatusRow = $projectStatusResult->fetch_assoc();
+
+	$class_name = str_replace(" ", "", strtolower($projectStatusRow['name']));
+
+	if($project_status_id != $projectRow['status']){
+		$permissionUpdate = "";
+		if($project_status_id == "6"){
+			$permissionUpdate = ",`rp_permission`='0',`wo_permission`='0'";
+		}
+		$updateSql = 'UPDATE `projects` SET `project_status`="' . $project_status_id . '" ' . $permissionUpdate . ' WHERE id="' . $project_id . '"';
+		$mysql->sqlordie($updateSql);
+		$insertSql = 'INSERT INTO `project_status` (`project_id`, `status_id`, `created_user`, `created_date`) VALUES ("' . $project_id . '", "' . $project_status_id . '", "' . $userID . '", NOW())';
+		$mysql->sqlordie($insertSql);
+		$html = '<button class="status status_' . $class_name . '" onclick="return false;"><span>' . $projectStatusRow['name'] . '</span></button>';
+	}else{
+		$html = '<button class="status status_' . $class_name . '" onclick="return false;"><span>' . $projectStatusRow['name'] . '</span></button>';
+	}
+	echo $html;
+
+?>
