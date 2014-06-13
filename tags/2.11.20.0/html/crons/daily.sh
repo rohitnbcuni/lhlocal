3520391badabeacad477af718b910642f521ee74
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2014-06-13 09:51:29 +0000

    creating new tag

---
+++ tags/2.11.20.0/html/crons/daily.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+stime=$(date +%c)
+echo $stime "Daily Cron started ..." >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+echo "########################" >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+php /var/www/lighthouse-uxd/lighthouse/current/html/crons/cron.updatewo.php
+if [ $? != 0 ]; then
+/bin/mail -s "Daily Cron Failed - cron.updatewo.php" ots-tools-support@nbcuni.com
+else
+echo "cron.updatewo.php exit status good" >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+fi
+
+php /var/www/lighthouse-uxd/lighthouse/current/html/crons/cron.perms.php
+if [ $? != 0 ]; then
+/bin/mail -s "15 Min Cron Failed - cron.perms.php" ots-tools-support@nbcuni.com
+else
+echo "cron.perms.php exit status good" >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+fi
+
+etime=$(date +%c)
+echo $etime "Daily Cron Completed ..." >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+echo "###########################" >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
+echo "" >> /var/www/lighthouse-uxd/lighthouse/current/html/crons/daily.log
