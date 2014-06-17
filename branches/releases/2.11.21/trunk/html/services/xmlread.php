a91933244a61037699ab898cd5feac1229e433e6
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2014-06-17 10:58:40 +0000

    createing new branch 2.11.21

---
+++ branches/releases/2.11.21/trunk/html/services/xmlread.php
@@ -0,0 +1,83 @@
+<?php 
+$url = 'http://ec2-75-101-162-191.compute-1.amazonaws.com:8080/solr/lighthouse_active/select?q='.urlencode($_GET['s']).'%20AND%20categories:quality'; 
+$ch = curl_init();
+		$request='<request>'.$request.'</request>';
+		curl_setopt($ch, CURLOPT_URL, $url);
+		if(defined('HTTP_PROXY') && HTTP_PROXY!='')
+		{
+			curl_setopt($ch,CURLOPT_PROXY,HTTP_PROXY);
+			curl_setopt($ch,CURLOPT_HTTPPROXYTUNNEL,true);
+			curl_setopt($ch,CURLOPT_PROXYTYPE,CURLPROXY_HTTP);
+		}
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt ($ch, CURLOPT_POST, 1);
+		curl_setopt ($ch, CURLOPT_POSTFIELDS, $request);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/xml', 'Content-Type: application/xml'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_USERPWD,BC_USER . ":" . BC_PASSWORD);
+		if(preg_match('/^(http)/',$url)) curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+    	$xml = (array)simplexml_load_string($response);
+	$titleDesc = array();
+	$desc = array();
+   	if(count($xml) > 0){
+	foreach($xml as $key =>$val){
+		if($key == 'result'){
+				$searchResult =  $val->doc;
+				if(count($searchResult) > 0){
+				foreach($searchResult as $k => $v){
+					if(count($searchResult) > 0){
+						foreach($searchResult as $sKey => $eVal){
+							$comments='';
+							for($i=0;$i<count($eVal->arr[1]->str);$i++)
+							{$no = $i+1;
+							$comments .= $no.'.'. $eVal->arr[1]->str[$i].'</br>';
+							 }
+											$id[] =  (array) $eVal->str[0];
+											$project_id[] = (array) $eVal->str[1];
+											$title[] = (array) $eVal->str[2];
+											$urllink[] = (array) $eVal->arr->str[1];
+											$desc[] = (array) $eVal->str[4];
+                                            						$cat[] = (array) $eVal->str[5];
+											$comment[] =  (array) $comments ;
+					
+						}
+					
+					}
+				}
+			
+			}
+		}
+	
+	}
+	
+	}	
+	
+	for($i =0 ; $i<count($id); $i++){
+?>
+
+<article class="answers type-answers status-publish">
+<?php if($cat[$i][0]=='quality'){$link='/quality/index/edit/?defect_id=';}else{$link='/workorders/index/edit/?wo_id=';}?>
+<h3 class="entry-title"><a href="<?php echo $link.$id[$i][0];?>"><?php echo $title[$i][0];?></a></h3>
+</header><!-- .entry-header -->
+<div class="entry-content native_html_style">
+<p><?php echo $desc[$i][0];?> <span><a href="<?php echo $link.$id[$i][0];?>" class="more-link">...read more</a></span></p>
+</div><!-- .entry-content -->
+
+<div class="entry-content native_html_style">
+<p>Comments:</br><span><?php echo $comment[$i][0];?> <span></p>
+</div><!-- .entry-content -->
+
+<footer class="entry-meta">
+<?php if ( $cat[$i][0]!='') { ?> 
+<span class="cat-links">
+<span class="entry-utility-prep entry-utility-prep-cat-links">Categories: </span><?php echo $cat[$i][0];?></span>
+<?php } ?>
+</footer><!-- #entry-meta -->
+</article>
+<?php  } 
+
+				/*******************solar code end ******************/ ?>
