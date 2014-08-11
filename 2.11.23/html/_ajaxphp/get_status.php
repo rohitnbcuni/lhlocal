2b895c611f34d181386e13d956f5789ababfb4ed
manoj.sharma <manoj.sharma@nbcuni.com>
2014-08-11 09:39:14 +0000

    createing new branch 2.11.23

---
+++ 2.11.23/html/_ajaxphp/get_status.php
@@ -0,0 +1,17 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	global $mysql;
+	$projId = $mysql->real_escape_string(@$_REQUEST['compid']);
+	$split = explode("_", $mysql->real_escape_string(@$_REQUEST['section']));
+
+	$query = "SELECT * FROM `project_brief_sections` WHERE `project_id`='$projId' AND `section_type` = '" .$split[1] ."' LIMIT 1";
+	$result = $mysql->sqlordie($query);
+	
+	if($result) {
+		$row = $result->fetch_assoc();
+		
+		echo $row['flag'];
+	}
+?>
