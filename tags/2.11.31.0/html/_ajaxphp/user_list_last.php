d74f4c3910403213d13e9fc890f0b6a2ec400605
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2015-06-29 10:00:14 +0000

    creating new tag

---
+++ tags/2.11.31.0/html/_ajaxphp/user_list_last.php
@@ -0,0 +1,33 @@
+<?PHP
+	include('../_inc/config.inc');
+	include("sessionHandler.php");
+	global $mysql;
+	//$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+	$html = '';
+	$wo_id = $mysql->real_escape_string($_GET['id']);
+	//$user_query = "SELECT id,last_name, first_name FROM `users` WHERE `company`='2' ORDER BY `last_name`";
+	$user_query = "SELECT id,last_name, first_name FROM `users` WHERE `active`='1' AND `deleted`='0'  ORDER BY `last_name`";
+	$user_result = $mysql->sqlordie($user_query);
+	$assigned_query= "SELECT `assigned_to` FROM `workorders` WHERE `id`=$wo_id";
+    $assigned_result = $mysql->sqlordie($assigned_query);
+	$assigned_row = $assigned_result->fetch_assoc();
+
+	if($user_result->num_rows > 0) {
+		while($row = $user_result->fetch_assoc()) {
+             if( $assigned_row['assigned_to'] == $row['id']){
+				 $selected = " SELECTED";
+			 }
+			 else {
+						$selected = "";
+					}
+			if(!empty($row['last_name'])){
+					$userName  = $row['last_name'] .", " .$row['first_name'];
+				}else{
+					$userName  = $row['first_name'];
+				}
+			$html .= '<option value="' .$row['id'] .'"' .$selected .' >' .ucfirst($userName) .'</option>';
+		}
+	}
+	
+	echo $html;
+?>
