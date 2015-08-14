b6f75b48d6cb17d644f5229529ed414cf6e1d05e
shobhitsingh.bhadauria@nbcuni.com <shobhitsingh.bhadauria@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2015-08-14 11:17:45 +0000

    creating new tag

---
+++ tags/2.11.32.0/html/healthCheck.php
@@ -0,0 +1,19 @@
+<?php
+
+include('_inc/config.inc');
+
+	
+	$link = mysql_connect(DB_SERVER.":".DB_PORT, DB_USERNAME, DB_PASSWORD);
+	if (!$link) {
+	    die('Could not connect: ');
+	}else{
+		echo 'GOOD';
+	}
+	
+	
+	// make DB_DATABASE the current db
+	$db_selected = mysql_select_db(DB_DATABASE, $link);
+	if (!$db_selected) {
+	    die ('Can\'t use Database : ');
+	}
+	mysql_close($link);
