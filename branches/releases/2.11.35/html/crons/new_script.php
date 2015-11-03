45ead5f9cd9963a6c009ec75840cc52b99912ab7
manoj.sharma@nbcuni.com <manoj.sharma@nbcuni.com@1b7f0e37-dc91-4cb0-99d4-63854bf1e1ba>
2015-11-03 09:47:15 +0000

    createing new branch 2.11.35

---
+++ branches/releases/2.11.35/html/crons/new_script.php
@@ -0,0 +1,30 @@
+<?PHP
+	/*include('/var/www/lighthouse-uxd/lighthouse/html/_inc/config.inc');
+	$mysql = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+	$selectSql = 'SELECT id, IFNULL(project_status, "0") AS Status FROM projects where archived="0"';
+	$selectResult = $mysql->query($selectSql);
+	if($selectResult->num_rows > 0) {
+		while($selectRow = $selectResult->fetch_assoc()){
+			if($selectRow['Status'] == "0"){
+				$updateSql = 'UPDATE `projects` SET `project_status`="1" WHERE `id`="' . $selectRow['id'] . '"';
+				$mysql->query($updateSql);
+				$insertSql = 'INSERT INTO `project_status` (`project_id`, `status_id`, `created_user`, `created_date`) VALUES ("' . $selectRow['id'] . '", "1", "83", NOW())';
+				$mysql->query($insertSql);
+			}
+		}
+	}
+
+	$selectSql = 'SELECT id, IFNULL(project_status, "0") AS Status FROM projects where archived="1"';
+	$selectResult = $mysql->query($selectSql);
+	if($selectResult->num_rows > 0) {
+		while($selectRow = $selectResult->fetch_assoc()){
+			if($selectRow['Status'] == "0"){
+				$updateSql = 'UPDATE `projects` SET `project_status`="6" WHERE `id`="' . $selectRow['id'] . '"';
+				$mysql->query($updateSql);
+				$insertSql = 'INSERT INTO `project_status` (`project_id`, `status_id`, `created_user`, `created_date`) VALUES ("' . $selectRow['id'] . '", "6", "83", NOW())';
+				$mysql->query($insertSql);
+			}
+		}
+	}*/
+?>
